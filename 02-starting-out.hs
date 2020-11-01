152c796d66ad5a09eeefc7610f3723879d4e96bc
Victor Zagorodny <viktor.zahorodnii@railsware.com>
2020-11-01 20:35:43 +0200

    Solved a few of the first examples for Chapter 2

--- 02-starting-out.hs
+++ 02-starting-out.hs
@@ -14,19 +14,20 @@ penultimate l = last (init l)
 
 -- Find the element at index k in list l
 -- For example: "findK 2 [0,0,1,0,0,0]" returns 1
-findK k l = undefined
+findK k l = l !! k
 
 -- Determine if list l is a palindrome
-isPalindrome l = undefined
+isPalindrome l = l == reverse l
+
 
 {-
  - Duplicate the elements in list xs, for example "duplicate [1,2,3]" would give the list [1,1,2,2,3,3]
- - Hint: The "concat [l]" function flattens a list of lists into a single list. 
+ - Hint: The "concat [l]" function flattens a list of lists into a single list.
  - (You can see the function definition by typing ":t concat" into the interpreter. Perhaps try this with other variables and functions)
  -
  - For example: concat [[1,2,3],[3,4,5]] returns [1,2,3,3,4,5]
  -}
-duplicate xs = undefined
+duplicate xs = concat (map (take 2 . repeat) xs)
 
 {-
  - Imitate the functinality of zip
