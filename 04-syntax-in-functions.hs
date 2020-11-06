49b47dbcf59f66ea5fcb0f778030ab57f7cc16ab
Victor Zagorodny <viktor.zahorodnii@railsware.com>
2020-11-06 09:50:33 +0200

    Chapter 4 exercises

--- 04-syntax-in-functions.hs
+++ 04-syntax-in-functions.hs
@@ -1,12 +1,17 @@
 -- This function should print a single digit number as English text, or "unknown" if it's out of the range 0-9
 englishDigit :: Int -> String
-englishDigit x = undefined
+englishDigit x
+  | x `elem` [0..9] = show x
+  | otherwise       = "unknown"
 
--- given a tuple, divide fst by snd, using pattern matching. 
+-- given a tuple, divide fst by snd, using pattern matching.
 -- it should return undefined for division by zero
 divTuple :: (Eq a, Fractional a) => (a, a) -> a
-divTuple (x, y) = undefined
+divTuple (x, y)
+  | y == 0    = undefined
+  | otherwise = x / y
 
 -- if the first three numbers in a list are all zero, return True
 threeZeroList :: [Int] -> Bool
-threeZeroList [x] = undefined
+threeZeroList (x:y:z:zs) = x == 0 && y == 0 && z == 0
+threeZeroList _          = False
